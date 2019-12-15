5f4930dac66afb3e6682d8ed8662c60d39a85893
Atle Magnussen <atlemagnussen@gmail.com>
2019-12-15 23:55:32 +0100

    added some sane colors

--- config.h
+++ config.h
@@ -8,10 +8,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#008b8b" },
+	/*               fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#004c4c" },
+	[SchemeSel] =  { "#eeeeee", "#006666" },
+	[SchemeOut] =  { "#000000", "#0000ff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
